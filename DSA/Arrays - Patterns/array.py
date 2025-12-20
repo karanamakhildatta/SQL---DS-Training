046ded1ef1f306b964e1aaf37028915d0875562b
karanamakhildatta <104244498+karanamakhildatta@users.noreply.github.com>
2025-12-20 17:38:02 +0530

    basic programes array

--- DSA/Arrays - Patterns/array.py
+++ DSA/Arrays - Patterns/array.py
@@ -1,4 +1,4 @@
-def find_sum(arr):
+def sum_of_array(arr):
     '''
     Calculate the sum of all elements in the array
     '''
@@ -46,5 +46,54 @@ def count_perfect_squares(arr):
             count += 1
     return count
 
+def find_total_of_perfect_squares(arr):
+    '''
+    Find total of perfect squares in array
+    '''
+    total = 0
+    for num in arr:
+        if num**0.5 == int(num**0.5):
+            total += num
+    return total
 
+def find_average(arr):
+    '''
+    Calculate the average of all elements in the array
+    '''
+    total = sum_of_array(arr)
+    number_of_elements = len(arr)
+    average = total / number_of_elements
+    return average
+
+# find count even numbers in array
+
+def count_even_numbers(arr):
+    '''
+    Find count of even numbers in array
+    '''
+    # even_number = number/2 => reminder == 0
+    # % => it gives reminder
+    count = 0
+    for num in arr:
+        if num % 2 == 0:
+            count += 1
+    return count
+
+def count_odd_numbers(arr):
+    pass
+def find_total_of_odd_numbers(arr):
+    pass
+def find_total_of_even_numbers(arr):
+    pass
+
+def reverse_array(arr):
+    '''
+    Reverse the array
+    '''
+    left  = 0
+    right = len(arr) - 1
+    while left < right:
+        arr[left],arr[right] = arr[right], arr[left]
+        left+=1
+        rigth-=1
 
